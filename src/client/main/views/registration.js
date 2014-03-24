8f94f5bdcfac844c83ff86311b5abee61e283cb4
cambell-prince <cambell.prince@gmail.com>
2014-03-24 15:47:26 +1300

    registrant form

--- src/client/main/views/registration.js
+++ src/client/main/views/registration.js
@@ -2,5 +2,53 @@
 
 angular.module('sgw.registration', [])
 	.controller('RegistrationCtrl', ['$scope', function($scope) {
+//		var data = {
+//			email: 'me@example.com',
+//			name: 'Some Name',
+//			paymentMethod: 'bank',
+//			attendees: [
+//			    {
+//			    	name: 'Some Camper',
+//			    	age: '14',
+//			    	gender: 'm'
+//			    }
+//			]
+//		};
+
+		var data = {};
+		
+		$scope.genders = [
+		    { id: 'm', label: 'Male'},
+		    { id: 'f', label: 'Female'}
+		];
+		
+		$scope.paymentMethods = [
+		    { id: 'ib', label: 'Internet Banking'},
+		    { id: 'cc', label: 'Credit Card'},
+		    { id: 'py', label: 'Pay at Church'}
+		];
+		
+		var reset = function() {
+			$scope.registrant = {};
+			$scope.addAttendee();
+		}
+				
+		$scope.addAttendee = function() {
+			var newAttendee = {
+				name: '',
+				age: '',
+				gender: ''
+			};
+			if ($scope.registrant.attendees == undefined) {
+				$scope.registrant.attendees = [];
+			}
+			$scope.registrant.attendees.push(newAttendee);
+		};
+		
+		$scope.removeAttendee = function($index) {
+			$scope.registrant.attendees.splice($index, 1);
+		};
+		
+		reset();
 		
 	}]);
